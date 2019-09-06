0d7ad03cbea7b4bb84684faf55d68090f1256c2e
Guillaume CAMANES <guillaume.camanes@niji.fr>
2019-09-06 10:32:37 +0200

    #2 added version management

--- tenant_config/index.js
+++
@@ -1,8 +0,0 @@
-import Config from 'react-native-config';
-
-const tenantConfigs = {
-  oca: require('./app1/config'),
-  soshca: require('./app2/config'),
-};
-
-export default tenantConfigs[Config.APP_PIN].default;
